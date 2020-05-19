fba65a18189313d827a1661520486e5415f26adc
Marj-11 <41150886+Marj-11@users.noreply.github.com>
2020-05-19 10:35:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const list = document.querySelectorAll('.listChild');
 
-if (window.location.href === `${window.origin}/prima/index.html`) {
+if (window.location.href === `${window.origin}/prima/Home/`) {
     list[0].classList.add('white');
 }
-if (window.location.href === `${window.origin}/prima/gallerie.html`) {
+if (window.location.href === `${window.origin}/prima/Gallerie/`) {
     list[1].classList.add('white');
 }
-if (window.location.href === `${window.origin}/prima/termine.html`) {
+if (window.location.href === `${window.origin}/prima/Termine/`) {
     list[2].classList.add('white');
 }
-if (window.location.href === `${window.origin}/prima/%C3%BCber_uns.html`) {
+if (window.location.href === `${window.origin}/prima/Uber/`) {
     list[3].classList.add('white');
 }
-if (window.location.href === `${window.origin}/prima/kontakt.html`) {
+if (window.location.href === `${window.origin}/prima/Kontakt/`) {
     list[4].classList.add('white');
 }
 const burger = document.querySelector('.burger');
